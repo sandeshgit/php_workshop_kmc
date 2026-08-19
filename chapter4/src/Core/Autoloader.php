226e9ac1dc40bd58732db960e21d45aa0328ba14
Sandesh Yadav <sandeshyadav.ydv@gmail.com>
2026-08-19 16:21:25 +0545

    Chapter 4 starting from chapter 3

---
+++ chapter4/src/Core/Autoloader.php
@@ -0,0 +1,21 @@
+<?php
+
+spl_autoload_register(function($className){
+  
+  $prefix = "App\\";
+  $baseDir = dirname(__DIR__) . '/';
+
+  if (strpos($className, $prefix) !== 0) {
+    return;
+  }
+
+  $relativeClass = substr($className, strlen($prefix));
+
+  $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+
+  if (file_exists($file)) {
+    require_once $file;
+  }
+
+ 
+});
---
+++ chapter4/src/Core/Database.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database{
+  private PDO $connection;
+  private static ?Database $instance = null;
+
+  private function __construct()
+  {
+    $dsn = "mysql:host=db;port=3306;dbname=php_workshop;charset=utf8mb4";
+
+    try {
+      $this->connection = new PDO($dsn, 'root', 'root');
+    } catch (PDOException $e) {
+      die("Database connection failed: " . $e->getMessage());
+    }
+  }
+
+  public static function getInstance() : self{
+    if (self::$instance == null) {
+      self::$instance = new self();
+    }
+    return self::$instance;
+  }
+
+  public function getConnection() :PDO {
+    return $this->connection;
+  }
+}
